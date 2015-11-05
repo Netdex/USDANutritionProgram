1bd4650f467887a60c4418169d95df3de25d4ddb
Vince Ou <vince.ou@gmail.com>
2015-11-04 20:08:43 -0500

    Created InfoPanel for each individual FoodItem

--- USDAProg/src/gui/InfoPanel.java
+++ USDAProg/src/gui/InfoPanel.java
@@ -1,5 +1,109 @@
 package gui;
 
-public class InfoPanel {
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import parser.parsables.FoodItem;
+
+public class InfoPanel extends JPanel {
+
+	SearchPanel searchPanel;
+	FoodItem food;
+	double amountOfFood;
+
+	public InfoPanel(FoodItem food, SearchPanel searchPanel) {
+		super();
+		this.food = food;
+		this.searchPanel = searchPanel;
+
+		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		JPanel header = new JPanel();
+
+		JButton backButton = new JButton();
+		try {
+			backButton.setIcon(new ImageIcon(ImageIO.read(new File(
+					"images/backButton.png"))));
+		} catch (IOException e) {
+		}
+
+		JButton moreInfo = new JButton();
+		try {
+			moreInfo.setIcon(new ImageIcon(ImageIO.read(new File(
+					"images/moreInfoButton.png"))));
+		} catch (IOException e) {
+		}
+
+		backButton.addActionListener(new BackButtonListener());
+		moreInfo.addActionListener(new MoreInfoButtonListener());
+
+		header.add(backButton);
+		header.add(moreInfo);
+
+		this.add(header);
+
+		JList<String> nutritionDataList = new JList<String>();
+		Font standardText = new Font(Font.SANS_SERIF, Font.BOLD, 14);
+
+		JLabel commonDescLabel = new JLabel(food.getLongDescription());
+		commonDescLabel.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 18));
+		nutritionDataList.add(commonDescLabel);
+
+		JLabel foodGroupLabel = new JLabel(food.getFoodGroup().toString());
+		foodGroupLabel.setFont(standardText);
+		nutritionDataList.add(foodGroupLabel);
+
+		JLabel commonNameLabel = new JLabel(food.getCommmonName());
+		commonNameLabel.setFont(standardText);
+		nutritionDataList.add(commonNameLabel);
+
+		JLabel unitsTitle = new JLabel(
+				"Units: enter how much of the food product you are intending to consume.");
+		unitsTitle.setFont(standardText);
+		nutritionDataList.add(unitsTitle);
+
+		/*
+		 * Get the number of weight measurement units for each food.
+		 * 
+		 * Use a for loop and generate that many text entry boxes, one for each
+		 * unit of measure
+		 * 
+		 * Convert the most recently entered value to the universal unit
+		 * 
+		 * Do math with all the nutrients and the universal unit to figure out
+		 * the displayed amount of nutrients
+		 */
+
+		this.add(nutritionDataList);
+	}
+
+	class BackButtonListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// "delete" this panel and show the searchPanel results again
+		}
+
+	}
+
+	class MoreInfoButtonListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			// draw the more info dialog over this
+		}
+
+	}
 
 }
