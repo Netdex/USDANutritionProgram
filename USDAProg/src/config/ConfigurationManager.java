33342955d0e50220d0270e78322cb42416c9b5c8
Netdex <netdex593@gmail.com>
2015-11-06 12:05:04 -0500

    Config overrides

--- USDAProg/src/config/ConfigurationManager.java
+++ USDAProg/src/config/ConfigurationManager.java
@@ -16,20 +16,32 @@ public class ConfigurationManager {
 		prop = new Properties();
 		config = new File(fileName);
 	}
-	
-	public ConfigurationManager(File file){
+
+	public ConfigurationManager(File file) {
 		prop = new Properties();
 		config = file;
 	}
 
-	public void addItem(String key, String value) {
-		prop.setProperty(key, value);
+	public void addItem(String key, Object value) {
+		prop.setProperty(key, value.toString());
 	}
 
-	public String getItem(String key) {
+	public String getString(String key) {
 		return prop.getProperty(key);
 	}
 
+	public int getInteger(String key) {
+		return Integer.parseInt(getString(key));
+	}
+
+	public double getDouble(String key) {
+		return Double.parseDouble(getString(key));
+	}
+
+	public boolean getBoolean(String key) {
+		return Boolean.parseBoolean(getString(key));
+	}
+	
 	public void load() {
 		try {
 			prop.load(new FileInputStream(config));
