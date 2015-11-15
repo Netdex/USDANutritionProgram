7aa0fe10ddb420156fecf24f67dc8af18e89d42a
Netdex <netdex593@gmail.com>
2015-11-15 13:52:50 -0500

    Fixed issue with grams

--- USDAProg/src/parser/parsables/WeightUnit.java
+++ USDAProg/src/parser/parsables/WeightUnit.java
@@ -33,8 +33,8 @@ public class WeightUnit implements Parsable<WeightUnit>, Formattable {
 	private static WeightUnit getGram() {
 		try {
 			return new WeightUnit()
-					.parse(new String[] { "-1", "1", "Gram", "1" });
-		} catch (InvalidParseDataException e) {
+                    .parse(new String[]{"-1", "", "1", "Gram", "1", "", ""});
+        } catch (InvalidParseDataException e) {
 			e.printStackTrace();
 		}
 		return null;
