008bf00c17d7e680c1d22bc14228ae521cabe904
Vince Ou <vince.ou@gmail.com>
2015-11-15 13:21:31 -0500

    Added method to create new "gram" WeightUnit.

--- USDAProg/src/parser/parsables/WeightUnit.java
+++ USDAProg/src/parser/parsables/WeightUnit.java
@@ -10,108 +10,122 @@ import parser.InvalidParseDataException;
  */
 public class WeightUnit implements Parsable<WeightUnit>, Formattable {
 
-    public static final int PARSE_DATA_LENGTH = 7;
-    public static WeightUnit GRAM = getGram();
-    public static WeightUnit SAMPLE = new WeightUnit();
-    private int ndbNo;
-    private int seq;
-    private double amount;
-    private String desc;
-    private double gramWeight;
-    private int numDataPts;
-    private double stdDev;
-    public WeightUnit() {
+	public static final int PARSE_DATA_LENGTH = 7;
+	public static WeightUnit GRAM = getGram();
+	public static WeightUnit SAMPLE = new WeightUnit();
+	private int ndbNo;
+	private int seq;
+	private double amount;
+	private String desc;
+	private double gramWeight;
+	private int numDataPts;
+	private double stdDev;
 
-    }
+	public WeightUnit() {
 
-    /**
-     * Gets a unit representing a gram
-     * @return a unit representing a gram
-     */
-    private static WeightUnit getGram() {
-        try {
-            return new WeightUnit().parse(new String[]{"-1", "1", "Gram", "1"});
-        } catch (InvalidParseDataException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+	}
 
-    /**
-     * @return the parseDataLength
-     */
-    public static int getParseDataLength() {
-        return PARSE_DATA_LENGTH;
-    }
+	/**
+	 * Gets a unit representing a gram
+	 * 
+	 * @return a unit representing a gram
+	 */
+	private static WeightUnit getGram() {
+		try {
+			return new WeightUnit()
+					.parse(new String[] { "-1", "1", "Gram", "1" });
+		} catch (InvalidParseDataException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 
-    @Override
-    public WeightUnit parse(String[] data) throws InvalidParseDataException {
-        if (data.length != PARSE_DATA_LENGTH)
-            throw new InvalidParseDataException();
-        ndbNo = Integer.parseInt(data[0]);
-        // seq = Integer.parseInt(data[1]);
-        amount = Double.parseDouble(data[2]);
-        desc = data[3];
-        gramWeight = Double.parseDouble(data[4]);
-        // numDataPts = data[5].equals("") ? 0 : Integer.parseInt(data[5]);
-        // stdDev = data[6].equals("") ? 0 : Double.parseDouble(data[6]);
-        return this;
-    }
+	/**
+	 * @return the parseDataLength
+	 */
+	public static int getParseDataLength() {
+		return PARSE_DATA_LENGTH;
+	}
 
-    @Override
-    public String getFormat() {
-        return Formattable.getFileFormatted(String.format("~%05d~", ndbNo), "0", amount + "", "~"
-                + desc + "~", gramWeight + "", "0", "0");
+	@Override
+	public WeightUnit parse(String[] data) throws InvalidParseDataException {
+		if (data.length != PARSE_DATA_LENGTH)
+			throw new InvalidParseDataException();
+		ndbNo = Integer.parseInt(data[0]);
+		// seq = Integer.parseInt(data[1]);
+		amount = Double.parseDouble(data[2]);
+		desc = data[3];
+		gramWeight = Double.parseDouble(data[4]);
+		// numDataPts = data[5].equals("") ? 0 : Integer.parseInt(data[5]);
+		// stdDev = data[6].equals("") ? 0 : Double.parseDouble(data[6]);
+		return this;
+	}
 
-    }
+	@Override
+	public String getFormat() {
+		return Formattable.getFileFormatted(String.format("~%05d~", ndbNo),
+				"0", amount + "", "~" + desc + "~", gramWeight + "", "0", "0");
 
-    public int getNDBNo() {
-        return ndbNo;
-    }
+	}
 
-    /**
-     * @return the seq
-     */
-    public int getSeq() {
-        return seq;
-    }
+	public int getNDBNo() {
+		return ndbNo;
+	}
 
-    /**
-     * @return the amount
-     */
-    public double getAmount() {
-        return amount;
-    }
+	/**
+	 * @return the seq
+	 */
+	public int getSeq() {
+		return seq;
+	}
 
-    /**
-     * @return the desc
-     */
-    public String getDesc() {
-        return desc;
-    }
+	/**
+	 * @return the amount
+	 */
+	public double getAmount() {
+		return amount;
+	}
 
-    /**
-     * @return the gramWeight
-     */
-    public double getGramWeight() {
-        return gramWeight;
-    }
+	/**
+	 * @return the desc
+	 */
+	public String getDesc() {
+		return desc;
+	}
 
-    /**
-     * @return the numDataPts
-     */
-    public int getNumDataPts() {
-        return numDataPts;
-    }
+	/**
+	 * @return the gramWeight
+	 */
+	public double getGramWeight() {
+		return gramWeight;
+	}
 
-    /**
-     * @return the stdDev
-     */
-    public double getStdDev() {
-        return stdDev;
-    }
+	/**
+	 * @return the numDataPts
+	 */
+	public int getNumDataPts() {
+		return numDataPts;
+	}
 
-    public String toString() {
-        return this.getAmount() + " " + this.getDesc();
-    }
+	/**
+	 * @return the stdDev
+	 */
+	public double getStdDev() {
+		return stdDev;
+	}
+
+	public String toString() {
+		return this.getAmount() + " " + this.getDesc();
+	}
+
+	public WeightUnit newGramUnit() {
+		try {
+			// TODO figure out what this all means
+			return this.parse(new String[] { "0", "0", "0", "gram", "1", "0",
+					"0" });
+		} catch (InvalidParseDataException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 }
