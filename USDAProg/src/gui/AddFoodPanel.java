e8822c09059dac226b616e49db5ca9418add4084
Netdex <netdex593@gmail.com>
2015-11-16 16:21:56 -0500

    Fixed footnotes not saving properly Fixed minor food panel things

--- USDAProg/src/gui/AddFoodPanel.java
+++ USDAProg/src/gui/AddFoodPanel.java
@@ -443,6 +443,7 @@ public class AddFoodPanel extends JPanel {
 								line.getNutrient().getNutrientNumber() + "",
 								line.getAmountForEntry() + "", "", "", "", "",
 								"", "", "", "", "", "", "", "", "", "", "" });
+						nut.setNutrientDescription(nut.getNutrientInfo());
 					} catch (InvalidParseDataException e1) {
 						e1.printStackTrace();
 					}
--- USDAProg/src/parser/parsables/Footnote.java
+++ USDAProg/src/parser/parsables/Footnote.java
@@ -6,33 +6,36 @@ import parser.InvalidParseDataException;
 public class Footnote implements Parsable<Footnote>, Formattable{
 
 	public static final int PARSE_DATA_LENGTH = 5;
+	private int ndbNo;
+	private int seqNo;
+	private Footnote.FootnoteType footntType;
+	private int nutrNo;
+	private String footnoteTxt;
+
 	@Override
 	public String getFormat() {
 		return Formattable.getFileFormatted(
 				String.format("~%05d~", ndbNo),
-				String.format("~%02d~", seqNo)
-//				String.format(, args)
+				String.format("~%02d~", seqNo),
+				"~~",
+				"~~",
+				footnoteTxt
 				);
 	}
+
 	@Override
 	public Footnote parse(String[] data) throws InvalidParseDataException {
 		if(data.length != PARSE_DATA_LENGTH)
 			throw new InvalidParseDataException();
 		ndbNo = Integer.parseInt(data[0]);
 		seqNo = Integer.parseInt(data[1]);
-		footntType = data[2].equals("D") 
-				? FootnoteType.FOOD_DESCRIPTION : data[2].equals("M") 
+		footntType = data[2].equals("D")
+				? FootnoteType.FOOD_DESCRIPTION : data[2].equals("M")
 				? FootnoteType.MEASURE_DESCRIPTION : FootnoteType.NUTRIENT_VALUE;
 		nutrNo = data[3].equals("") ? 0 : Integer.parseInt(data[3]);
 		footnoteTxt = data[4];
 		return this;
 	}
-	
-	private int ndbNo;
-	private int seqNo;
-	private Footnote.FootnoteType footntType;
-	private int nutrNo;
-	private String footnoteTxt;
 	
 	/**
 	 * @return The nutrient databank number
